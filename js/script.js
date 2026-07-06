7e11deba14834494d1e3fece5b0983abb8553b8b
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-07-06 12:19:02 +0530

    practice on js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+// const arr = ["haseeb", "rahul", "Nikhil", 20, 40, 60];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === arr[5] ) {
+//         console.log(arr[i]);
+//     }
+// }
+
+// const arr = [2, 4, 6, 8, 10];
+
+// function customFunction(item){
+//     return item * 2;
+// }
+
+// const newArray = arr.map(customFunction);
+
+// console.log(newArray);
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result = arr.slice(0, 8)
+
+console.log(result);
+
+// const animals = ["ant", "bison", "camel", "duck", "elephant"];
+
+// console.log(animals.slice(2));
+
+
+
+
